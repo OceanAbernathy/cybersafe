cd0a2fc39724f158f6fc3037c66b06fc65bcfde9
Ocean Abernathy <ocean.abernathy@gmail.com>
2019-11-05 14:41:48 -0600

    Updated nav animation for mobile

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("menu-links");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+$(document).ready(function(){
+    $('.icon').click(function(){
+        $('body').toggleClass("menu-open");
+    });
+});
